4834b530cd29bc445662063e366834da88d2565c
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-05-18 00:34:18 -0500

    Update process_news_dataset.py

--- examples/classifier/data/real_fake_news/process_news_dataset.py
+++ examples/classifier/data/real_fake_news/process_news_dataset.py
@@ -8,8 +8,8 @@ out_df = pd.DataFrame(data=dict())
 for field in ['title', 'text', 'label']:
     out_df[field] = df[field]
 
-s1 = list(df['title'])
-s2 = list(df['text'])
+s1 = list(out_df['title'])
+s2 = list(out_df['text'])
 out_df['titletext'] = [s1[i] + " " + s2[i] for i in range(len(df))]
 
 split = [0.8, 0.1, 0.1]
