4eb01f874f1ed35140ae5d0b5892180ca7c53545
patricia_xiao <PatriciaXiao@users.noreply.github.com>
2021-05-18 00:31:10 -0500

    updates

--- examples/classifier/data/real_fake_news/process_news_dataset.py
+++ examples/classifier/data/real_fake_news/process_news_dataset.py
@@ -1,14 +1,16 @@
 import pandas as pd 
 
+MAX_SEQ_LEN = 128
+
 df = pd.read_csv("news.csv")
 
 out_df = pd.DataFrame(data=dict())
 for field in ['title', 'text', 'label']:
-    out_df[field] = df[field]
+    out_df[field] = [" ".join(s.split()[:MAX_SEQ_LEN]) for s in df[field]]
 
 s1 = list(df['title'])
 s2 = list(df['text'])
-out_df['titletext'] = [s1[i] + " " + s2[i] for i in range(len(df))]
+out_df['titletext'] = [" ".join((s1[i] + " " + s2[i]).split()[:MAX_SEQ_LEN]) for i in range(len(df))]
 
 split = [0.8, 0.1, 0.1]
 n = len(out_df)
